ee8d085b0c667054d4ca44470c75d2a2539eaa4f
fuming <18310005980@163.com>
2018-09-18 18:46:04 +0800

    加入适配6.0

--- app/src/main/java/com/zwonline/top28/activity/SplashActivity.java
+++ app/src/main/java/com/zwonline/top28/activity/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.zwonline.top28.activity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -16,6 +20,9 @@ import com.qq.e.ads.splash.SplashADListener;
 import com.qq.e.comm.util.AdError;
 import com.zwonline.top28.R;
 import com.zwonline.top28.module.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SplashActivity extends Activity implements SplashADListener {
@@ -37,7 +44,13 @@ public class SplashActivity extends Activity implements SplashADListener {
         container = (ViewGroup) this.findViewById(R.id.splash_container);
         skipView = (TextView) findViewById(R.id.skip_view);
         splashHolder = (ImageView) findViewById(R.id.splash_holder);
-        fetchSplashAD(this, container, skipView, Constants.APPID, Constants.SplashPosID, this, 3);
+        // 如果targetSDKVersion >= 23，就要申请好权限。如果您的App没有适配到Android6.0（即targetSDKVersion < 23），那么只需要在这里直接调用fetchSplashAD接口。
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkAndRequestPermission();
+        } else {
+            // 如果是Android6.0以下的机器，默认在安装时获得了所有权限，可以直接调用SDK
+            fetchSplashAD(this, container, skipView, Constants.APPID, Constants.SplashPosID, this, 3);
+        }
     }
 
     /**
@@ -136,6 +149,42 @@ public class SplashActivity extends Activity implements SplashADListener {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+    /**
+     *
+     * ----------非常重要----------
+     *
+     * Android6.0以上的权限适配简单示例：
+     *
+     * 如果targetSDKVersion >= 23，那么必须要申请到所需要的权限，再调用广点通SDK，否则广点通SDK不会工作。
+     *
+     * Demo代码里是一个基本的权限申请示例，请开发者根据自己的场景合理地编写这部分代码来实现权限申请。
+     * 注意：下面的`checkSelfPermission`和`requestPermissions`方法都是在Android6.0的SDK中增加的API，如果您的App还没有适配到Android6.0以上，则不需要调用这些方法，直接调用广点通SDK即可。
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkAndRequestPermission() {
+        List<String> lackedPermission = new ArrayList<String>();
+        if (!(checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED)) {
+            lackedPermission.add(Manifest.permission.READ_PHONE_STATE);
+        }
+
+        if (!(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
+            lackedPermission.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (!(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
+            lackedPermission.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        // 权限都已经有了，那么直接调用SDK
+        if (lackedPermission.size() == 0) {
+            fetchSplashAD(this, container, skipView, Constants.APPID,  Constants.SplashPosID, this, 0);
+        } else {
+            // 请求所缺少的权限，在onRequestPermissionsResult中再看是否获得权限，如果获得权限就可以调用SDK，否则不要调用SDK。
+            String[] requestPermissions = new String[lackedPermission.size()];
+            lackedPermission.toArray(requestPermissions);
+            requestPermissions(requestPermissions, 1024);
+        }
     }
 
 }
